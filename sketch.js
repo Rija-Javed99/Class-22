8ea1ce4e9e6991e3f8a3e60dbb3514ea8a4dfcda
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-02-20 13:57:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  
 }
 
 function draw() {
-  background(255,255,255);  
+  background();  
   drawSprites();
 }
